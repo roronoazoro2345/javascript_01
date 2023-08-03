65c58e7d63d8210252ad627b70384ce834de752e
roronoazoro2345 <141061103+roronoazoro2345@users.noreply.github.com>
2023-08-03 03:13:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS as new version
+let name="rahul"
+let age=12
+let isloggedIn=true
+
+// alert(3+2)  // we are using nodejs not browser
+// data types
+//number = 2 to the power 53 for more than this use bigInt
+// string= always in ""
+//boolean=true/false
+//null = standalone value
+//undefined
+//symbol=unique
+console.log(typeof "Aniket")
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isloggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
