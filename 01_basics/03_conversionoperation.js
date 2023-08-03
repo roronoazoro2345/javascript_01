257decd06f4b9ef20a6051f0a1e794edfbbd55bc
roronoazoro2345 <141061103+roronoazoro2345@users.noreply.github.com>
2023-08-03 03:36:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score=33
+let score1="33abc"
+console.log(typeof score)
+console.log(typeof score1)
+let valueInNumber=Number(score1)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let IsLoggedIn=""
+let IsNotLogged="aniket"
+let IsBoolean=Boolean(IsNotLogged)
+console.log(IsLoggedIn)
+console.log(IsNotLogged)
+console.log(IsBoolean)
+
+//33=33
+// "33abc"=NaN
+//true=1;fslse=0
+//null=0
+//undefined=NaN
+//""=false
+//"aniket"=true
