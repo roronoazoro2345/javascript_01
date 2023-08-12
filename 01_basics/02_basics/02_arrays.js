919e44fc2ffc2fe56d03b2c079b6a9d67ce7d3b3
roronoazoro2345 <141061103+roronoazoro2345@users.noreply.github.com>
2023-08-12 03:44:20 +0000

    array_part_2

---
+++ 01_basics/02_basics/02_arrays.js
@@ -0,0 +1,19 @@
+const marvel_heroes=["Thor","Ironman","Hulk"]
+const dc=["Superman","flash","batman"]
+//marvel_heroes.push(dc)
+//console.log(marvel_heroes)
+//console.log(marvel_heroes[3][1])
+const all_heroes=marvel_heroes.concat(dc)
+//console.log(all_heroes)
+const all_new_heroes=[...marvel_heroes,...dc]
+console.log(all_new_heroes)
+const real_array=[1,2,3[4,5,6],7,[6,[7,8]]]
+const real_array_1=real_array.flat(Infinity)
+console.log(real_array_1)
+console.log(Array.isArray("Aniket"))
+console.log(Array.from("Aniket"))
+console.log(Array.from({name:"aniket"}))//intresting
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
